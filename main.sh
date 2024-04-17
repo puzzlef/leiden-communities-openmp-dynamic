2a93e2a589c33140b894673e03ab6a9b5ebeadf8
Subhajit Sahu <wolfram77@gmail.com>
2024-04-17 15:42:44 +0530

    :bug: setup top level details

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="leiden-communities-openmp"
+src="leiden-communities-openmp-dynamic"
 out="$HOME/Logs/$src$1.log"
 ulimit -s unlimited
 printf "" > "$out"
@@ -12,33 +12,44 @@ if [[ "$DOWNLOAD" != "0" ]]; then
 fi
 
 # Fixed config
-# export OMP_STACKSIZE="4G"
 : "${TYPE:=float}"
-: "${MAX_THREADS:=64}"
-: "${REPEAT_METHOD:=5}"
+: "${MAX_THREADS:=2}"
+: "${REPEAT_BATCH:=5}"
+: "${REPEAT_METHOD:=1}"
+# Parameter sweep for batch (randomly generated)
+: "${BATCH_UNIT:=%}"
+: "${BATCH_LENGTH:=1}"
+: "${BATCH_DELETIONS_BEGIN:=0.00000005}"
+: "${BATCH_DELETIONS_END:=0.05}"
+: "${BATCH_DELETIONS_STEP:=*=10}"
+: "${BATCH_INSERTIONS_BEGIN:=0.00000005}"
+: "${BATCH_INSERTIONS_END:=0.05}"
+: "${BATCH_INSERTIONS_STEP:=*=10}"
+# Parameter sweep for number of threads
+: "${NUM_THREADS_MODE:=all}"
+: "${NUM_THREADS_BEGIN:=64}"
+: "${NUM_THREADS_END:=64}"
+: "${NUM_THREADS_STEP:=*=2}"
 # Define macros (dont forget to add here)
 DEFINES=(""
 "-DTYPE=$TYPE"
 "-DMAX_THREADS=$MAX_THREADS"
+"-DREPEAT_BATCH=$REPEAT_BATCH"
 "-DREPEAT_METHOD=$REPEAT_METHOD"
+"-DBATCH_UNIT=\"$BATCH_UNIT\""
+"-DBATCH_LENGTH=$BATCH_LENGTH"
+"-DBATCH_DELETIONS_BEGIN=$BATCH_DELETIONS_BEGIN"
+"-DBATCH_DELETIONS_END=$BATCH_DELETIONS_END"
+"-DBATCH_DELETIONS_STEP=$BATCH_DELETIONS_STEP"
+"-DBATCH_INSERTIONS_BEGIN=$BATCH_INSERTIONS_BEGIN"
+"-DBATCH_INSERTIONS_END=$BATCH_INSERTIONS_END"
+"-DBATCH_INSERTIONS_STEP=$BATCH_INSERTIONS_STEP"
+"-DNUM_THREADS_MODE=\"$NUM_THREADS_MODE\""
+"-DNUM_THREADS_BEGIN=$NUM_THREADS_BEGIN"
+"-DNUM_THREADS_END=$NUM_THREADS_END"
+"-DNUM_THREADS_STEP=$NUM_THREADS_STEP"
 )
 
 # Run
 g++ ${DEFINES[*]} -std=c++17 -O3 -fopenmp main.cxx
-# stdbuf --output=L ./a.out ~/Data/web-Stanford.mtx   0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/indochina-2004.mtx  0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/uk-2002.mtx         0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx     0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/uk-2005.mtx         0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/webbase-2001.mtx    0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/it-2004.mtx         0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sk-2005.mtx         0 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/com-LiveJournal.mtx 1 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/com-Orkut.mtx       1 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/asia_osm.mtx        1 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/europe_osm.mtx      1 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/kmer_A2a.mtx        1 0 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/kmer_V1r.mtx        1 0 2>&1 | tee -a "$out"
-
-# Signal completion
-curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
+stdbuf --output=L ./a.out 2>&1 | tee -a "$out"
